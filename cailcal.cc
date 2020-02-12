baf5bfe6b2473d78d17e2822e4e63f4a7c75c3a0
jamie <jamiecaille190@gmail.com>
2020-02-12 13:31:57 -0500

    some stuff lol

--- cailcal.cc
+++ cailcal.cc
@@ -1,36 +1,67 @@
 #include <string>
 #include <iostream>
+#include <map>
+#include <vector>
+#include <fstream>
 using namespace std;
 
-string months[12] = {
-  "JAN",
-  "FEB",
-  "MAR",
-  "APR",
-  "MAY",
-  "JUN",
-  "JUL",
-  "AUG",
-  "SEP",
-  "OCT",
-  "NOV",
-  "DEC"};
+vector<pair<string, int>> months = {
+  make_pair<string,int>("JAN", 31),
+  make_pair<string,int>("FEB", 29),
+  make_pair<string,int>("MAR", 31),
+  make_pair<string,int>("APR", 30),
+  make_pair<string,int>("MAY", 31),
+  make_pair<string,int>("JUN", 30),
+  make_pair<string,int>("JUL", 31),
+  make_pair<string,int>("AUG", 31),
+  make_pair<string,int>("SEP", 30),
+  make_pair<string,int>("OCT", 31),
+  make_pair<string,int>("NOV", 30),
+  make_pair<string,int>("DEC", 31),
+};
 
-int main()
+int main(int argc, char** argv)
 {
-  for(int i = 1; i <= 3; i++)
-  {
-    for(int j = 1; j <= 31; j++)
-    {
-      cout << "" + months[i] << " " << j << endl;
-    }
-  }
 
-  /*for(int i = JAN; i <= DEC; i++)
+  string output = "";
+
+  /*bool setBegin = false;
+  bool setEnd = false;
+  int beginMonth = 0;
+  int beginDay = 0;
+  int endMonth = 0;
+  int endDay = 0;
+  for(int args = 0; args < argc; args++)
   {
-    for(int i = 1; i <= 31; i++)
+
+  }*/
+
+  /*int newline = 0; //actual one, below one is for my use
+  for(auto i : months)
+  {
+    for(int j = 1; j <= i.second; ++j)
     {
-      
+      cout << i.first << " " << j << "\t\t\t\t\t\t";
+      if(newline == 3) cout << "\n\n\n\n\n";
+      newline = (++newline % 4);
     }
   }*/
+
+
+  int newline = 0;
+  for(int i = 1; i <= 3; i++)
+  {
+    for(int j = 1; j < months.at(i).second; ++j)
+    {
+      output += months.at(i).first + " " + to_string(j) + "\t\t\t\t\t\t";
+      if(newline == 3) output += "\n\n\n\n\n";
+      newline = (++newline % 4);
+    }
+  }
+  ofstream output_file;
+  output_file.open("cailcal.txt");
+  output_file << output;
+  output_file.close();
+  
+  return 0;
 }

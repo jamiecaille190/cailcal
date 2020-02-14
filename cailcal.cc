b23ed6bf43f6aa6d7eb1e430448dc383d672bedd
Jamie Caille <jamiecaille190@gmail.com>
2020-02-14 01:21:06 -0500

    help me

--- cailcal.cc
+++ cailcal.cc
@@ -21,72 +21,20 @@ vector<pair<string, int>> months = {
   make_pair<string,int>("DEC", 31),
 };
 
+int begin_month = 1;
+int begin_day = 1;
+int end_month = 12;
+int end_day = 31;
+string file_name = "cal.txt";
+string tab_string = "\t";
+string enter_string = "\n";
+
+int resolve_flags(int argc, char** argv); //TODO: move into .h file
+
 int main(int argc, char** argv)
 {
-  int op;
-  int begin_month = 1;
-  int begin_day = 1;
-  int end_month = 12;
-  int end_day = 31;
-  string date;
-  while((op = getopt(argc, argv, "b:e:")) != -1)
-  {
-    switch(op) {
-      case 'b':
-        date = optarg;
-        if(date.length() != 4)
-        {
-          cerr << "Length of begin date invalid, use mmdd format." << endl;
-          return 1;
-        }
-        else
-        {
-          begin_month = stoi(date.substr(0, 2));
-          if(begin_month < 1 || begin_month > 12)
-          {
-            cerr << "Begin date month must be between 1 (JAN) and 12 (DEC)." << endl;
-            return 1;
-          }
-          begin_day = stoi(date.substr(2, 2));
-          if(begin_day < 1 || begin_day > months.at(begin_month - 1).second)
-          {
-            cerr << "Begin date day must be between 1 and the amount of days in the month." << endl;
-            return 1;
-          }
-        }
-        
-        break;
-      case 'e':
-        date = optarg;
-        if(date.length() != 4)
-        {
-          cerr << "Length of end date invalid, use mmdd format." << endl;
-          return 1;
-        }
-        else
-        {
-          end_month = stoi(date.substr(0, 2));
-          if(end_month < 1 || end_month > 12)
-          {
-            cerr << "End date month must be between 1 (JAN) and 12 (DEC)." << endl;
-            return 1;
-          }
-          end_day = stoi(date.substr(2, 2));
-          if(end_day < 1 || end_day > months.at(end_month - 1).second)
-          {
-            cerr << "End date day must be between 1 and the amount of days in the month." << endl;
-            return 1;
-          }
-        }
-        if(begin_month == end_month && begin_day == end_day)
-        {
-          cerr << "Begin date can't be the same as end date." << endl;
-          return 1;
-        }
 
-        break;
-    }
-  }
+  if(resolve_flags(argc, argv) == 1) return 1; //TODO: fix later to do exceptions
 
   string output = "";
   begin_month --;
@@ -114,18 +62,117 @@ int main(int argc, char** argv)
     if(month_counter == end_month && day_counter == end_day + 1) break;
     day_counter = 1;
     month_counter ++;
-    if(month_counter > 11) month_counter = 0;
-    
-    last_day = months.at(month_counter).second;
-    if(month_counter == end_month) last_day = end_day;
+    if(month_counter > 11) month_counter = 0; // december, maybe change all values like this to an enum?
 
     month_cur = months.at(month_counter).first;
+    
+    if(month_counter == end_month) last_day = end_day;
+    else last_day = months.at(month_counter).second;
   }
 
   ofstream output_file;
-  output_file.open("cal.txt");
+  output_file.open(file_name + ".txt");
   output_file << output;
   output_file.close();
   
   return 0;
+}
+
+int resolve_flags(int argc, char** argv)
+{
+  int op;
+  string date;
+  while((op = getopt(argc, argv, "b:e:t:x:y:")) != -1)
+  {
+    switch(op) {
+      case 'b':
+        date = optarg;
+        if(date.length() != 4)
+        {
+          cerr << "Length of begin date invalid, use mmdd format." << endl;
+          return 1;
+        }
+        else
+        {
+          begin_month = stoi(date.substr(0, 2));
+          if(begin_month < 1 || begin_month > 12)
+          {
+            cerr << "Begin date month must be between 1 (JAN) and 12 (DEC)." << endl;
+            return 1;
+          }
+          begin_day = stoi(date.substr(2, 2));
+          if(begin_day < 1 || begin_day > months.at(begin_month - 1).second)
+          {
+            cerr << "Begin date day must be between 1 and the amount of days in the month." << endl;
+            return 1;
+          }
+        }
+        
+        break;
+      case 'e': 
+        date = optarg;
+        if(date.length() != 4)
+        {
+          cerr << "Length of end date invalid, use mmdd format." << endl;
+          return 1;
+        }
+        else
+        {
+          end_month = stoi(date.substr(0, 2));
+          if(end_month < 1 || end_month > 12)
+          {
+            cerr << "End date month must be between 1 (JAN) and 12 (DEC)." << endl;
+            return 1;
+          }
+          end_day = stoi(date.substr(2, 2));
+          if(end_day < 1 || end_day > months.at(end_month - 1).second)
+          {
+            cerr << "End date day must be between 1 and the amount of days in the month." << endl;
+            return 1;
+          }
+        }
+        if(begin_month == end_month && begin_day == end_day)
+        {
+          cerr << "Begin date can't be the same as end date." << endl;
+          return 1;
+        }
+
+        break; 
+      case 't':
+        file_name = optarg;
+        for(auto i : file_name)
+        {
+          if(!isalpha(i))
+          {
+            cerr << "File name must be all alphabetical characters." << endl;
+            return 1;
+          }
+        }
+        break; 
+      case 'x':
+        string pp = optarg;
+        for(auto i : pp)
+        {
+          if(!isalpha(i))
+          {
+            cerr << "File name must be all alphabetical characters." << endl;
+            return 1;
+          }
+        }
+        pp = pp;
+        break;
+      case 'y': 
+        string qq = optarg;
+        for(auto i : qq)
+        {
+          if(!isalpha(i))
+          {
+            cerr << "File name must be all alphabetical characters." << endl;
+            return 1;
+          }
+        }
+        qq = qq;
+        break; 
+    }
+  }
 }

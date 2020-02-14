90bbd4955415b03d2311ef23f2bd1c3101aae44d
jamie <jamiecaille190@gmail.com>
2020-02-14 13:58:45 -0500

    cleaned up

--- Makefile
+++ Makefile
@@ -1,3 +1,25 @@
-++=G++
-cailcalmake: cailcal.cc
-	g++ -o cailcal cailcal.cc
+IDIR=include/
+LDIR=lib/
+ODIR=obj/
+
+PP=g++
+FLAGS=-Wall -I $(IDIR)
+
+_DEPS=cailcal.h
+DEPS=$(patsubst %, $(IDIR)%, $(_DEPS))
+
+_OBJS=cailcal.o
+OBJS=$(patsubst %, $(ODIR)%, $(_OBJS))
+
+TARGET=cailcal
+
+$(ODIR)%.o: $(LDIR)%.cc $(DEPS)
+	$(PP) -c -o $@ $< $(FLAGS)
+
+$(TARGET): $(OBJS)
+	$(PP) -o $@ $^ $(FLAGS)
+
+.PHONY: clean
+
+clean:
+	rm -f $(ODIR)*.o $(TARGET)
--- lib/cailcal.cc
+++ lib/cailcal.cc
@@ -1,33 +1,10 @@
-#include <string>
+#include "cailcal.h"
+
 #include <iostream>
-#include <map>
-#include <vector>
 #include <fstream>
 #include <unistd.h>
+
 using namespace std;
-
-vector<pair<string, int>> months = {
-  make_pair<string,int>("JAN", 31),
-  make_pair<string,int>("FEB", 28),
-  make_pair<string,int>("MAR", 31),
-  make_pair<string,int>("APR", 30),
-  make_pair<string,int>("MAY", 31),
-  make_pair<string,int>("JUN", 30),
-  make_pair<string,int>("JUL", 31),
-  make_pair<string,int>("AUG", 31),
-  make_pair<string,int>("SEP", 30),
-  make_pair<string,int>("OCT", 31),
-  make_pair<string,int>("NOV", 30),
-  make_pair<string,int>("DEC", 31),
-};
-
-int begin_month = 1;
-int begin_day = 1;
-int end_month = 12;
-int end_day = 31;
-string file_name = "cal.txt";
-string tab_string = "\t";
-string enter_string = "\n";
 
 int resolve_flags(int argc, char** argv); //TODO: move into .h file
 
@@ -149,30 +126,7 @@ int resolve_flags(int argc, char** argv)
           }
         }
         break; 
-      case 'x':
-        string pp = optarg;
-        for(auto i : pp)
-        {
-          if(!isalpha(i))
-          {
-            cerr << "File name must be all alphabetical characters." << endl;
-            return 1;
-          }
-        }
-        pp = pp;
-        break;
-      case 'y': 
-        string qq = optarg;
-        for(auto i : qq)
-        {
-          if(!isalpha(i))
-          {
-            cerr << "File name must be all alphabetical characters." << endl;
-            return 1;
-          }
-        }
-        qq = qq;
-        break; 
     }
   }
+  return 0;
 }
